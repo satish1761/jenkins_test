fb85b5577ca6526e4ebdc3c1725fb1de4631c86f
satish <mudirajsatish5@gmail.com>
2021-07-09 20:03:23 +0530

    push check

--- src/main/java/com/satish/jenkins_pro/App.java
+++ src/main/java/com/satish/jenkins_pro/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! pullin check " );
+        System.out.println( "push check " );
     }
 }
